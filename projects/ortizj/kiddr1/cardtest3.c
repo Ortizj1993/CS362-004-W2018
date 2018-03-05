412e8b6abf62847aca0c856d6d4f75760bc92083
Ortizj1993 <35379306+Ortizj1993@users.noreply.github.com>
2018-03-04 20:42:01 -0800

    Update cardtest3.c

--- projects/ortizj/kiddr1/cardtest3.c
+++ projects/ortizj/kiddr1/cardtest3.c
@@ -51,7 +51,7 @@ int main() {
 	// copy the game state to a test case
 	memcpy(&testG, &G, sizeof(struct gameState));
 
-	playSmithy(&testG,struct gameState *state, int handPos);
+	playSmithy(&testG, handpos, thisPlayer);
 
 	//Test that player drew three cards
 	newCards = 3;
